0d2d27ca2e8cc80cab4a248a00aaa787fcdc883f
Adrien Boutigny <adrien@slash-tmp.dev>
2024-05-13 21:15:40 +0200

    feat(api): log http requests

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import morgan from 'morgan';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'common'));
   await app.listen(4000);
 }
 bootstrap();
