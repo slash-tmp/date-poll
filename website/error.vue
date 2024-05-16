eaf28b0b47285f09a1b03488dd7a45ba4dc95d32
Quentin Bellanger <hello@quentin-bellanger.>
2024-05-16 10:31:48 +0200

    organize poll admin page + add error page

---
+++ website/error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps<{
+  error: NuxtError;
+}>();
+</script>
+
+<template>
+  <div>
+    <h2>{{ error.statusMessage }}</h2>
+    <p>{{ error.message }}</p>
+    <NuxtLink :to="{ name: 'index' }">
+      {{ $t("pages.error.homeLink") }}
+    </NuxtLink>
+  </div>
+</template>
--- website/pages/poll/admin/[id].vue
+++ website/pages/poll/admin/[id].vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import type { AdminPollApiResponse } from "~/types/poll";
 
-const route = useRoute();
+const { t } = useI18n();
 
+const route = useRoute();
 const id = route.params.id;
 
+// Fetch poll
 const { data: poll } = await useFetch<AdminPollApiResponse>(
   `/api/polls/admin/${id}`,
 );
@@ -12,14 +14,90 @@ const { data: poll } = await useFetch<AdminPollApiResponse>(
 if (!poll.value) {
   throw createError({
     statusCode: 404,
-    // FIXME: i18n
-    statusMessage: "Poll not found",
+    statusMessage: t("pages.poll.admin.id.error.404.title"),
+    message: t("pages.poll.admin.id.error.404.description"),
   });
+}
+
+// TODO: Delete poll
+function deletePoll() {
+  console.log("delete poll");
+}
+
+// Copy share link
+const shareUrl = computed(() => {
+  return `/poll/${poll.value?.publicUid}`;
+});
+
+const shareLinkInputRef = ref<HTMLInputElement>();
+const showCopySuccess = ref(false);
+
+async function copyLink() {
+  await navigator.clipboard.writeText(shareUrl.value);
+  showCopySuccess.value = true;
+
+  setTimeout(() => {
+    showCopySuccess.value = false;
+  }, 5000);
 }
 </script>
 
 <template>
-  <h1>{{ $t("pages.poll.admin.id.title", { id }) }}</h1>
+  <template v-if="poll">
+    <h1>{{ poll.title }}</h1>
 
-  <pre><code>{{ JSON.stringify(poll, null, 2) }}</code></pre>
+    <div>
+      <NuxtLink to="#">{{ $t("pages.poll.admin.id.meta.edit") }}</NuxtLink>
+      <br />
+      <button @click="deletePoll">
+        {{ $t("pages.poll.admin.id.meta.delete") }}
+      </button>
+    </div>
+
+    <div>
+      <ul>
+        <li v-if="poll.adminName">
+          {{ $t("pages.poll.admin.id.meta.createdBy") }}
+          <strong>{{ poll.adminName }}</strong>
+        </li>
+        <!-- TODO: add creation date -->
+        <li>
+          {{ $t("pages.poll.admin.id.meta.createdAt") }}
+          <strong>{{ "XX/XX/XXXX" }}</strong>
+        </li>
+        <li v-if="poll.description">{{ poll.description }}</li>
+      </ul>
+
+      <div>
+        <h2>{{ $t("pages.poll.admin.id.share.title") }}</h2>
+        <p>
+          {{ $t("pages.poll.admin.id.share.description") }}
+        </p>
+
+        <div aria-live="polite" role="alert">
+          <div>
+            <label for="share-link">{{
+              $t("pages.poll.admin.id.share.label")
+            }}</label>
+            <input
+              id="share-link"
+              ref="shareLinkInputRef"
+              type="url"
+              readonly
+              :value="shareUrl"
+            />
+          </div>
+          <button @click="copyLink">
+            {{ $t("pages.poll.admin.id.share.button") }}
+          </button>
+          <p v-if="showCopySuccess">
+            {{ $t("pages.poll.admin.id.share.successAlert") }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h2>{{ $t("pages.poll.admin.id.responses.title") }}</h2>
+    </div>
+  </template>
 </template>
