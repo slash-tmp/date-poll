39c6493f41cfeecbc1369771ef3e1a502ce4273c
Quentin Bellanger <hello@quentin-bellanger.>
2024-06-04 10:37:39 +0200

    update new poll tests

--- cypress/e2e/date-poll.cy.ts
+++ cypress/e2e/date-poll.cy.ts
@@ -13,7 +13,6 @@ describe("Poll creation page /poll/new", () => {
     cy.contains("Suivant").click();
 
     // Step 2
-    cy.contains("Ajouter une date").click();
     cy.getByLabel("Date n°1").type("2024-03-23");
     cy.getByLabel("Horaire n°1").type("08:30");
 
@@ -53,7 +52,7 @@ describe("Poll creation page /poll/new", () => {
 
     // Step should not change and an error message should be displayed
     cy.contains("Étape 2 sur 4");
-    cy.contains("Vous devez au moins ajouter une date.");
+    cy.focused().should("have.attr", "id", "input-choice-date-0");
   });
 
   it("saves data between steps", () => {
@@ -65,7 +64,6 @@ describe("Poll creation page /poll/new", () => {
     cy.contains("Suivant").click();
 
     // Step 2
-    cy.contains("Ajouter une date").click();
     cy.getByLabel("Date n°1").type("2024-03-23");
     cy.getByLabel("Horaire n°1").type("08:30");
     cy.contains("Précédent").click();
